a8c1bf7456856db9988da234a33b8bd808275912
asdred <smvber@gmail.com>
2018-02-02 13:36:21 +0000

    route

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const config = require('./config.json'),
 	  express = require('express'),
 	  bodyParser = require('body-parser'),
 	  methodOverride = require('method-override'),
-	  morgan = require('morgan'),
-	  routes = require('./routes');
+	  morgan = require('morgan');
+	  //routes = require('./routes');
 
 const app = express();
 
@@ -21,29 +21,11 @@ app.use(bodyParser.json({type: 'application/vnd.api+json'}));	// parse applicati
 app.use(methodOverride('X-HTTP-Method-Override'));				// override with the X-HTTP-Method-Override header in the request
 
 // Маршруты
-routes(app);
+//routes(app);
 
-/*
-// Вот это должно делаться тоже через routes
 app.get('/', function(req, res) {
 	res.sendfile('./public/index.html');
 });
-
-// Ловим 404 и пробрасываем в следующий обработчик
-app.use(function (req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
-
-// error handler
-// no stacktraces leaked to user unless in development environment
-app.use(function (err, req, res, next) {
-	res.status(err.status || 500);
-
-	res.status(err.status || 500)
-		.send(err.status ? err.message : 'Internal server error');
-});*/
 
 app.listen(config.server.port, () => {
 	console.log('Server listening at http://localhost:%s', config.server.port);
